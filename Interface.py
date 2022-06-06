9423d32a83044396b4013bba578552cbe7881cff
MicaDias <103238950+MicaDias@users.noreply.github.com>
2022-06-06 00:46:31 -0300

    Delete Interface.py

--- Interface.py
+++
@@ -1,8 +0,0 @@
-from zope.interface import Interface
-class Interfaz(Interface):
-    def insertarElemento(elemento,pos):
-        pass
-    def agregarElemento(elemento):
-        pass
-    def mostrarElemento(pos):
-        pass
